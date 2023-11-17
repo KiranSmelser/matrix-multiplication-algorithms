a6b17d0d239a3280deaba03b34ec116ba6a4477e
EliJ121315 <121189370+EliJ121315@users.noreply.github.com>
2023-11-17 13:50:03 -0700

    Add files via upload

--- SquareTestcases.java
+++ SquareTestcases.java
@@ -1,5 +1,9 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Arrays;
 import java.util.Random;
+
 
 public class SquareTestcases {
     // Function to perform matrix multiplication
@@ -37,15 +41,36 @@ public class SquareTestcases {
             System.out.println(Arrays.toString(row));
         }
     }
+    
 
     public static void main(String[] args) {
-        int minMatrixSize = 2;
         int maxMatrixSize = 1024;
         Random random = new Random();
+                
+        String filePath = "C:\\Users\\elij1\\Downloads\\TestOutput.csv";
+
+        
+        try (PrintWriter writer = new PrintWriter(new FileWriter(filePath))) {
+            // Write the header to the CSV file
+            writer.println("Coppersmith-Winograd runtime (nanoseconds),Strassen for square matrices runtime (nanoseconds),Strassen for non-square matrices runtime (nanoseconds)");
 
         for (int size = 2; size <= maxMatrixSize; size *= 2) {
             int[][] A = new int[size][size];
             int[][] B = new int[size][size];
+            
+            // Non-Square matrices
+//            int nonSquareSize1 = size*2-1;
+//            int nonSquareSize2 = size/2+1;
+//            int[][] C = new int[nonSquareSize1][nonSquareSize1];
+//            int[][] D = new int[nonSquareSize2][nonSquareSize2];
+            
+            // Fill non-square matrices C and D with random values
+//            for (int i = 0; i < nonSquareSize1; i++) {
+//                for (int j = 0; j < nonSquareSize2; j++) {
+//                    C[i][j] = random.nextInt(10);
+//                    D[i][j] = random.nextInt(10);
+//                }
+//            }
 
             // Fill matrices A and B with random values
             for (int i = 0; i < size; i++) {
@@ -54,23 +79,39 @@ public class SquareTestcases {
                     B[i][j] = random.nextInt(10);
                 }
             }
+            
+            long strassenSquareStartTime = System.nanoTime();
+            int[][] strassenSquareResult = StrassensMatrixMultiplication.multiply_matrix(A, B);
+            long strassenSquareEndTime = System.nanoTime();
 
             long copperStartTime = System.nanoTime();
             int[][] copperResult = CoppersmithWinograd.matrixMultiply(A, B);
             long copperEndTime = System.nanoTime();
 
-            long strassenSquareStartTime = System.nanoTime();
-            int[][] strassenSquareResult = CoppersmithWinograd.matrixMultiply(A, B);
-            long strassenSquareEndTime = System.nanoTime();
+            
+            
+//            long NonSquareStartTime = System.nanoTime();
+//            int[][] NonSquareResult = Non_Square.NonSquare(A, B);
+//            long NonSquareEndTime = System.nanoTime();
 
             // Check the accuracy of the result
             int[][] referenceResult = matrixMultiplication(A, B); // Iterative matrix multiplication for comparison
             boolean isEqual = checkEquality(copperResult, referenceResult) && checkEquality(strassenSquareResult, referenceResult);
+            
+            writer.println(String.valueOf(copperEndTime - copperStartTime) + "," +
+            		String.valueOf(strassenSquareEndTime - strassenSquareStartTime));// + "," +
+            		//String.valueOf(NonSquareEndTime - NonSquareStartTime));
+            
 
             System.out.println("Matrix size: " + size + "x" + size);
             System.out.println("Coppersmith-Winograd runtime (nanoseconds): " + (copperEndTime - copperStartTime));
             System.out.println("Strassen for square matrices runtime (nanoseconds): " + (strassenSquareEndTime - strassenSquareStartTime));
             System.out.println("Results are accurate: " + isEqual);
         }
+        
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
     }
 }
