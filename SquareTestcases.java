5d6b85dd77e469a8f036cf54f450daf8c946506b
EliJ121315 <121189370+EliJ121315@users.noreply.github.com>
2023-11-20 16:28:59 -0700

    Update SquareTestcases.java for Strassen's and Winograd's

--- SquareTestcases.java
+++ SquareTestcases.java
@@ -2,6 +2,7 @@ import java.util.Arrays;
 import java.util.Random;
 
 public class SquareTestcases {
+	
     // Function to perform matrix multiplication
     public static int[][] matrixMultiplication(int[][] A, int[][] B) {
         int n = A.length;
@@ -34,13 +35,119 @@ public class SquareTestcases {
     // Function to print a 2D array
     public static void printMatrix(int[][] matrix) {
         for (int[] row : matrix) {
-            System.out.println(Arrays.toString(row));
+            System.out.print(Arrays.toString(row));
         }
+    }
+    
+    public static String arrayToStringSideBySide(int[][] array1, int[][] array2, int[][] array3) {
+        StringBuilder result = new StringBuilder();
+
+        int maxLength = Math.max(Math.max(array1.length, array2.length), array3.length);
+
+        for (int i = -1; i < maxLength; i++) {
+            if (i < array1.length) {
+            	if (i == -1) {
+            		result.append("Iterative:");
+            		if (arrayRowToString(array1[0]).length() > 10) {
+	            		for (int j = 0; j < arrayRowToString(array1[0]).length() - 10; j++)
+	            			result.append(" ");
+	            		result.append("\t\t");
+            		}
+            		else
+            			result.append("\t");
+            	}
+            	else {
+            		result.append(arrayRowToString(array1[i]));
+            		result.append("\t\t");
+            	}            		
+            }
+            
+
+            if (i < array2.length) {
+            	if (i == -1) {
+            		result.append("Strassen's:");
+            		if (arrayRowToString(array1[0]).length() > 10) {
+	            		for (int j = 0; j < arrayRowToString(array1[0]).length() -11; j++)
+	            			result.append(" ");
+	            		result.append("\t\t");
+            		}
+            		else
+            			result.append("\t");
+            	}
+            	else {
+            		result.append(arrayRowToString(array2[i]));
+            		result.append("\t\t");
+            	}
+            }
+
+            if (i < array3.length) {
+            	if (i == -1) {
+            		result.append("Winograd's:");
+            	}
+            	else
+            		result.append(arrayRowToString(array3[i]));
+            }
+            result.append("\n");
+        }
+        return result.toString();
+    }
+    
+    
+    public static String arrayToStringSideBySide(int[][] array1, int[][] array2) {
+        StringBuilder result = new StringBuilder();
+
+        int maxLength = Math.max(array1.length, array2.length);
+
+        for (int i = -1; i < maxLength; i++) {
+            if (i < array1.length) {
+            	if (i == -1) {
+            		result.append("Matrix A:");
+            		if (arrayRowToString(array1[0]).length() > 10) {
+	            		for (int j = 0; j < arrayRowToString(array1[0]).length() - 9; j++)
+	            			result.append(" ");
+	            		result.append("\t\t");
+            		}
+            		else
+            			result.append("\t");
+            	}
+            	else {
+            		result.append(arrayRowToString(array1[i]));
+            		result.append("\t\t");
+            	}            		
+            }
+
+            if (i < array2.length) {
+            	if (i == -1) {
+            		result.append("Matrix B:");
+            	}
+            	else
+            		result.append(arrayRowToString(array2[i]));
+            }
+            result.append("\n");
+        }
+
+        return result.toString();
+    }
+    
+    // Returns the array row as a string
+    private static String arrayRowToString(int[] row) {
+        StringBuilder rowString = new StringBuilder();
+
+        for (int j = 0; j < row.length; j++) {
+            rowString.append(row[j]);
+            if (j < row.length - 1) {
+                rowString.append(", ");
+                if (row[j] < 100)
+                	rowString.append(" ");
+                if (row[j] < 10)
+                	rowString.append(" ");
+            }
+        }
+        return rowString.toString();
     }
 
     public static void main(String[] args) {
-        int minMatrixSize = 2;
-        int maxMatrixSize = 1024;
+        int maxMatrixSize = 8;
         Random random = new Random();
 
         for (int size = 2; size <= maxMatrixSize; size *= 2) {
@@ -55,22 +162,21 @@ public class SquareTestcases {
                 }
             }
 
-            long copperStartTime = System.nanoTime();
-            int[][] copperResult = CoppersmithWinograd.matrixMultiply(A, B);
-            long copperEndTime = System.nanoTime();
-
-            long strassenSquareStartTime = System.nanoTime();
-            int[][] strassenSquareResult = CoppersmithWinograd.matrixMultiply(A, B);
-            long strassenSquareEndTime = System.nanoTime();
+            int[][] StrassenResult = StrassensMatrixMultiplication.multiply_matrix(A, B);
+            
+            int[][] WinogradResult = CoppersmithWinograd.matrixMultiply(A, B);
 
             // Check the accuracy of the result
             int[][] referenceResult = matrixMultiplication(A, B); // Iterative matrix multiplication for comparison
-            boolean isEqual = checkEquality(copperResult, referenceResult) && checkEquality(strassenSquareResult, referenceResult);
-
+            
+            boolean isEqual = checkEquality(StrassenResult, referenceResult) && checkEquality(WinogradResult, referenceResult);
+            
+            // Print results
             System.out.println("Matrix size: " + size + "x" + size);
-            System.out.println("Coppersmith-Winograd runtime (nanoseconds): " + (copperEndTime - copperStartTime));
-            System.out.println("Strassen for square matrices runtime (nanoseconds): " + (strassenSquareEndTime - strassenSquareStartTime));
-            System.out.println("Results are accurate: " + isEqual);
+            System.out.print(arrayToStringSideBySide(A,B));
+            System.out.println("\nResults:");
+            System.out.print(arrayToStringSideBySide(referenceResult,StrassenResult,WinogradResult));
+            System.out.println("Results are accurate: " + isEqual + "\n\n");
         }
     }
 }
